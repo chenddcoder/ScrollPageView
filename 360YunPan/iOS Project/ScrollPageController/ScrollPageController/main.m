1a793dba33368c87b20d8da80cadceff8b8770d0
chenddcoder <chenddcoder@foxmail.com>
2015-06-05 16:02:49 +0800

    first upload

---
+++ 360YunPan/iOS Project/ScrollPageController/ScrollPageController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollPageController
+//
+//  Created by chendd on 15/6/3.
+//  Copyright (c) 2015年 icfcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
